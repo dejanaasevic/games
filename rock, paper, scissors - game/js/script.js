a312b181972699dce8bdee19e49fc13ac2bd46d6
dejana šević <96137453+dejanaasevic@users.noreply.github.com>
2022-07-04 13:46:22 +0200

    Add files via upload

--- rock, paper, scissors - game/js/script.js
+++ rock, paper, scissors - game/js/script.js
@@ -2,6 +2,9 @@ const userChoiceDisplay= document.getElementById('player-choice');
 const computerChoiceDisplay=document.getElementById('computer-choice');
 const resultDisplay =document.getElementById('result');
 const possibleChoices=document.getElementsByClassName('choice');
+const titleDisplay=document.getElementById('gametitle');
+const instructionDisplay=document.getElementById('instruction');
+    
 
 var energycount=3;
 let energyDisplay;
@@ -10,14 +13,45 @@ let computerChoice;
 let computerChoiceImageSrc;
 let result;
 
-for(var i=0; i<possibleChoices.length; i++){
-    possibleChoices[i].addEventListener('click',(e)=>{
+if(energycount===3 || energycount===2 || energycount===1){
+   initializeGame();
+}
+if(energycount===0){
+    gameOver();
+}
+
+
+function gameOver(){
+    titleDisplay.innerHTML='game over';
+    instructionDisplay.innerHTML='click the button to play again!';
+    
+    const btnDisplay=document.getElementById('againbtn');
+    let btn =document.createElement('button');
+    btn.innerHTML='start again!';
+    btn.addEventListener('click',(e)=>{
+        energycount=3;
+        energyDisplay='your energy: ♡ ♡ ♡';
+        document.getElementById('energy').innerHTML=energyDisplay;
+        titleDisplay.innerHTML='rock, paper, scissors';
+        instructionDisplay.innerHTML='how to play: choose one of the options below';
+        resultDisplay.innerHTML='result:';
+        btnDisplay.removeChild(btn);
+    }
+);
+    
+    btnDisplay.appendChild(btn);
+    
+  }
+
+
+function initializeGame(){
+    for(var i=0; i<possibleChoices.length; i++){ possibleChoices[i].addEventListener('click',(e)=>{
         userChoice=e.target.id;
         userChoiceDisplay.innerHTML=userChoice;
         generateComputerChoice();
     })
+  }
 }
-
 
 function generateComputerChoice(){
     var randomNumber= Math.floor(Math.random()*possibleChoices.length);
@@ -54,11 +88,9 @@ function generateResult(){
 function generateNewEnergy(){
     energycount--;
     switch(energycount){
-        case 2: energyDisplay='your energy: ♡ ♡'; break;
-        case 1: energyDisplay='your energy: ♡'; break;
-        default:
-            energyDisplay='your energy: /';
-            break;
+        case 2: energyDisplay='your energy: ♡ ♡';  break;
+        case 1: energyDisplay='your energy: ♡';    break;
+        default: energyDisplay='your energy: /'; gameOver(); break;
     }
     document.getElementById('energy').innerHTML=energyDisplay;
 }
