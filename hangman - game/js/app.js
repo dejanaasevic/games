d7a46886e3997db6cdc3a3cd00cf291e6cb6b51d
dejana šević <96137453+dejanaasevic@users.noreply.github.com>
2022-07-28 19:32:46 +0200

    Add files via upload

--- hangman - game/js/app.js
+++ hangman - game/js/app.js
@@ -1,48 +1,65 @@
-let letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; 
+let letters = 'A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z'; 
 const passwordChoice = [
-    'Better late than never',
-    'Actions speak louder than words',
-    'Don’t judge a book by it’s cover',
-    'Good things come to those who wait',
-    'If you can’t beat them, join them',
-    'Don’t bite the hand that feeds you',
-    'Rome wasn’t built in a day',
-    'Out of sight, out of mind',
-    'Easy come, easy go',
-    'The forbidden fruit is always the sweetest',
-    'Learn to walk before you run',
-    'The early bird catches the worm',
-    'Where there’s a will, there’s a way',
-    'All good things come to an end',
-    'An apple a day keeps the doctor away',
-    'Better safe than sorry',
-    'It’s no use crying over spilt milk',
-    'Laughter is the best medicine',
-    'The apple doesn’t fall far from the tree',
-    'Forgive and forget',
-    'Money doesn’t grow on trees',
-    'It’s easy to be wise after the event',
-    'It’s not over till it’s over',
-    'Never test the depth of the water with both feet',
-    'No gain without pain',
-    'The show must go on',
-    'Two heads are better than one',
-    'It’s better to be safe than sorry',
-    'You can’t always get what you want',
-    'While the cats away, the mice will play',
-    'Where one door shuts, another opens',
-    'Two wrongs don’t make a right'
+    'BETTER LATE THAN NEVER',
+    'ACTIONS SPEAK LOUDER THAN WORDS',
+    'DONT JUDGE A BOOK BY ITS COVER',
+    'GOOD THINGS COME TO THOSE WHO WAIT',
+    'IF YOU CANT BEAT THEM, JOIN THEM',
+    'DONT BITE THE HAND THAT FEEDS YOU',
+    'ROME WASNT BUILT IN A DAY',
+    'OUT OF SIGHT, OUT OF MIND',
+    'EASY COME, EASY GO',
+    'THE FORBIDDEN FRUIT IS ALWAYS THE SWEETEST',
+    'LEARN TO WALK BEFORE YOU RUN',
+    'THE EARLY BIRD CATCHES THE WORM',
+    'WHERE THERE IS A WILL, THERE IS A WAY',
+    'ALL GOOD THINGS COME TO AN END',
+    'AN APPLE A DAY KEEPS THE DOCTOR AWAY',
+    'BETTER SAFE THAN SORRY',
+    'LAUGHTER IS THE BEST MEDICINE',
+    'THE APPLE DOESNT FALL FAR FROM THE TREE',
+    'FORGIVE AND FORGET',
+    'MONEY DOESNT GROW ON TREES',
+    'ITS EASY TO BE WISE AFTER THE EVENT',
+    'ITS NOT OVER TILL ITS OVER',
+    'NEVER TEST THE DEPTH OF THE WATER WITH BOTH FEET',
+    'NO GAIN WITHOUT PAIN',
+    'THE SHOW MUST GO ON',
+    'TWO HEADS ARE BETTER THAN ONE',
+    'YOU CANT ALWAYS GET WHAT YOU WANT',
+    'WHILE THE CATS AWAY, THE MICE WILL PLAY',
+    'WHERE ONE DOOR SHUTS, ANOTHER OPENS',
+    'TWO WRONGS DONT MAKE A RIGHT'
 ];
 
 const alphabetDisplay = document.getElementById('aphabet');
 const hangmanPicture = document.getElementById('hangmanpicture');
-const timerDisplay = document.getElementById('timer');
 const passwordDisplay = document.getElementById('board');
-let password;
+const instructionsDisplay = document.getElementById('instructions');
+const startAgainButton = document.getElementById('startagain');
+const timerDisplay = document.getElementById('timerdisplay');
+const audioIcon = document.getElementById('audio');
+let password;  let generatedPassword; let mistakecount;
+
+
+let audioPlay = true;
+let audioYay = new Audio('tracks/yay.mp3'); let yayCount = 0;
+let audioWhat = new Audio('tracks/what.mp3'); let whatCount = 0;
+
+let currentTime; let startTimer;
+setInterval(updateTimer, 1000);
+
+startAgainButton.addEventListener('click', startAgain);
 
 initialiseGame();
 
 function initialiseGame(){
+    mistakecount=0; currentTime = 0; startTimer = false;
+    instructionsDisplay.innerHTML = "how to play: try to guess the phrase by choosing the letters";
+    hangmanPicture.src ="img/0.png";
+    timerDisplay.innerHTML = "00:00:00";
+    alphabetDisplay.innerHTML = "";
+    audioIcon.src = "img/audio.png";
     generateAlphabet();
     let random = Math.floor(Math.random() * passwordChoice.length);
     password = passwordChoice [random];
@@ -50,10 +67,9 @@ function initialiseGame(){
 }
 
 function generateBoard(){
-    let generatedPassword='';
+        generatedPassword='';
         for (let i = 0; i < password.length; i++){
             if (password[i] === ' ') { generatedPassword += ' '; }
-            else if (password[i] === '’') { generatedPassword += '’'; }
             else if (password[i] === ',') { generatedPassword += ','; }
             else { generatedPassword += '_'; }
         }
@@ -61,5 +77,96 @@ function generateBoard(){
 }
 
 function generateAlphabet(){
+    const letterArray = letters.split(',');
+    for(let i = 0; i<letterArray.length; i++){
+        const newLetter = document.createElement('div');
+        newLetter.innerHTML = letterArray[i];
+        newLetter.setAttribute('id',letterArray[i]);
+        newLetter.setAttribute('class', 'letter');
+        newLetter.addEventListener('click',checkLetter);
+        alphabetDisplay.appendChild(newLetter);
+    }
+}    
+
+function checkLetter(){
+    let found = false;  startTimer = true;
+    let selectedLetter = this.getAttribute('id');
+    let clickedletter = document.getElementById(selectedLetter);
     
+    clickedletter.style.backgroundColor = 'black';
+    clickedletter.style.color = 'white';
+    clickedletter.removeEventListener('click',checkLetter);
+    
+    console.log(password);
+    console.log(generatedPassword);
+    
+    let newPassword = '';
+    for(let i=0; i<password.length;i++){
+        if(password[i] === selectedLetter){
+            newPassword += selectedLetter;
+            audioYay.play();
+            found = true;
+        }
+        else{newPassword+=generatedPassword[i];}
+    }
+    
+    if(found == true){
+        generatedPassword = newPassword;
+        passwordDisplay.innerHTML = generatedPassword;
+        if(generatedPassword === password){
+            won();
+        }
+    }
+    
+    else if (mistakecount <=5){
+        mistakecount++;
+        hangmanPicture.src = "img/" + mistakecount + ".png";
+        if(mistakecount === 6){
+           gameOver();
+        }
+    }
+            console.log(newPassword);
+}
+
+function updateTimer(){
+    currentTime++;
+    if(startTimer === true){
+    let time = currentTime;
+    let hours = Math.floor(time/3600); time %= 3600;
+    let minutes = Math.floor(time/60); 
+    let seconds = Math.floor(time%60);
+    if(hours < 10){ hours = "0" + hours;}
+    if(minutes < 10){ minutes = "0" + minutes;}
+    if(seconds<10){seconds= "0" + seconds;}
+    timerDisplay.innerHTML = hours + ":" + minutes + ":" + seconds;
+    }
+}
+function gameOver(){
+    instructionsDisplay.innerHTML ="you just lost the game, press the button below and start again";
+    const allLetters = alphabetDisplay.querySelectorAll('div');
+    for(var i = 0; i<allLetters.length; i++){
+        alphabetDisplay.removeChild(allLetters[i]);
+    }
+    alphabetDisplay.innerHTML = "you lost";
+    startTimer = false;
+}
+
+function startAgain(){
+    const allLetters = alphabetDisplay.querySelectorAll('div');
+    for(var i = 0; i<allLetters.length; i++){
+        alphabetDisplay.removeChild(allLetters[i]);
+    }
+    startTimer = false;
+    initialiseGame();
+    
+}
+
+function won(){
+    instructionsDisplay.innerHTML ="press the button below and start again";
+    const allLetters = alphabetDisplay.querySelectorAll('div');
+    for(var i = 0; i<allLetters.length; i++){
+        alphabetDisplay.removeChild(allLetters[i]);
+    }
+    startTimer = false;
+    alphabetDisplay.innerHTML = "congratulations you won!";
 }
